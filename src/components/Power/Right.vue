f3b43f7a8f655df75b1ec55ee6766b1d94b97a1a
maatakli <369257776@qq.com>
2020-11-16 14:35:22 +0800

    第一次提交

---
+++ src/components/Power/Right.vue
@@ -0,0 +1,20 @@
+<!--  -->
+<template>
+  <div>
+    权限列表
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  }
+}
+
+</script>
+
+<style lang='less' scoped>
+
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,37 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Login from '../components/Login.vue'
+import Home from '../components/Home.vue'
+import Welcome from '../components/Welcome.vue'
+import User from '../components/user/User.vue'
+import Right from '../components/Power/Right.vue'
+
+Vue.use(Router)
+
+Vue.config.productionTip = false
+
+const router = new Router({
+  routes: [
+    { path: '/', redirect: '/login' },
+    { path: '/login', component: Login },
+    {
+      path: '/home',
+      component: Home,
+      redirect: '/welcome',
+      children: [
+        { path: '/welcome', component: Welcome },
+        { path: '/users', component: User },
+        { path: '/right', component: Right }
+      ]
+    }
+  ]
+})
+router.beforeEach((to, from, next) => {
+  if (to.path === '/login') return next()
+  // 获取token
+  const tokenStr = window.sessionStorage.getItem('token')
+  if (!tokenStr) return next('/login')
+  next()
+})
+
+export default router
