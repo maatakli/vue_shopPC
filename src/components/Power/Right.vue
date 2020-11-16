15e3317cb55dde6ee34c3edbc7a536b78b249542
maatakli <369257776@qq.com>
2020-11-16 14:38:31 +0800

    Right

--- src/components/Power/Right.vue
+++ src/components/Power/Right.vue
@@ -1,7 +1,12 @@
 <!--  -->
 <template>
   <div>
-    权限列表
+    <el-breadcrumb separator="/">
+      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item><a href="/">权限管理</a></el-breadcrumb-item>
+      <el-breadcrumb-item>权限列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <el-card></el-card>
   </div>
 </template>
 
